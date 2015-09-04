11b728a6c940183308577de09da4d5b1d128bd57
ssalteko <ssalteko@gmail.com>
2015-09-04 11:57:50 -0700

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -1,2 +1,8 @@
 #Problem 1 for Project Euler
 
+#from project Euler 9/4/2015
+def PE1(a, b, n):
+    def f(x, n):
+        fl = int((n-1)/x)
+        return x * fl * (fl+1)
+    return (f(a,n) + f(b,n) - f(a*b,n))/2
