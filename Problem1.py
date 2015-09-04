6a86626ed891aba186b932e4fbb788d1c889ced7
ssalteko <ssalteko@gmail.com>
2015-09-04 12:00:06 -0700

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -1,8 +1,10 @@
 #Problem 1 for Project Euler
 
+#This is my favorite python solution. There are no loops, because this solution requires no loops.
+
 #from project Euler 9/4/2015
 def PE1(a, b, n):
     def f(x, n):
         fl = int((n-1)/x)
         return x * fl * (fl+1)
-    return (f(a,n) + f(b,n) - f(a*b,n))/2
+    return (f(a,n) + f(b,n) - f(a*b,n))//2
