224afa394279d73ba4cc35cc7d292832818f7569
ssalteko <ssalteko@gmail.com>
2015-09-04 13:33:11 -0700

    Create Problem2.py

---
+++ Problem2.py
@@ -0,0 +1,10 @@
+#Problem 2 for Project Euler
+
+#Sum the even fibionocci numbers below 4M.
+
+def EvenFibSum(n):
+	f1,f2,EFS = 1,1,0
+	while (EFS <n):
+		EFS += (f1 + f2)
+		f1, f2 = f1 + 2 * f2, 2 * f1 + 3 * f2
+	return EFS
