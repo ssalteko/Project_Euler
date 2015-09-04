06d8de5f43c993253b9e0b44e91676a67264f946
ssalteko <ssalteko@gmail.com>
2015-09-04 13:14:58 -0700

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -2,9 +2,20 @@
 
 #This is my favorite python solution. There are no loops, because this solution requires no loops.
 
+#I also enjoy the functions within functions.
+
 #from project Euler 9/4/2015
 def PE1(a, b, n):
     def f(x, n):
         fl = int((n-1)/x)
         return x * fl * (fl+1)
     return (f(a,n) + f(b,n) - f(a*b,n))//2
+
+
+#What we see in f, is our desired multiple being multiplied by the sum of the natural numbers 
+#that index the number of those multiples below our limit.
+
+#3+6+9+...+999 = 3(1+2+3+...+333)
+#5+10+15+...+995 = 5(1+2+3+...+199)
+
+#Then we simply add the union of the sets, and subtract their intersection resulting in the sum of two sets.
