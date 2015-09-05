b72a3f359a89ad3dc3e52cd66d025cf8bc5aa5bc
ssalteko <ssalteko@gmail.com>
2015-09-05 00:38:34 -0700

    Create Problem003.py

---
+++ Problem003.py
@@ -0,0 +1,17 @@
+#Problem 3 from Project Euler
+#This was from the forum authored by rivermaker
+
+
+n = 317584931803
+
+f = 3
+
+
+while ( n > 1 ):
+    if n % f == 0:
+        n /= f
+    else:
+        f += 2
+
+
+print ('largest factor is', f)
