11cc8e3a4ba4c36b0c33ca539fe34844a0c34566
ssalteko <ssalteko@gmail.com>
2015-09-06 23:47:57 -0700

    Update Problem001.py
    The sum of all multiples of 3, and 5 below 1000.

--- Problem001.py
+++ Problem001.py
@@ -1,15 +1,19 @@
 #Problem 1 for Project Euler
-
-#This is my favorite python solution. There are no loops, because this solution requires no loops.
-
-#I also enjoy the functions within functions.
+#The goal is to find the sum of all multiples of 3, and 5 below 1000.
 
 #from project Euler 9/4/2015
 def PE1(a, b, n):
+    '''Returns the sum of all multiples of a and b, below the positive integer n.'''
     def f(x, n):
         fl = int((n-1)/x)
         return x * fl * (fl+1)
     return (f(a,n) + f(b,n) - f(a*b,n))//2
+
+print(3,5,1000)
+
+#This is my favorite python solution. There are no loops, because this solution requires no loops.
+
+#I also enjoy the functions within functions.
 
 
 #What we see in f, is our desired multiple being multiplied by the sum of the natural numbers 
