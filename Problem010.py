8c5a174a986bc87ded09ac674fe781f2fdeac718
ssalteko <ssalteko@gmail.com>
2015-09-07 14:44:25 -0700

    Create Problem010.py

---
+++ Problem010.py
@@ -0,0 +1,13 @@
+#find the sum of primes below 2,0000
+
+def sumprime(n):
+    '''returns the nth prime'''
+    primes = [2,3,5,7,11,13]
+    num = 15
+    while len(primes) < n+1:
+        if all(num % m != 0 for m in primes):
+            primes.append(num)
+        num += 2
+    return primes[n]
+
+#print(nprime[10000])
