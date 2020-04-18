b10e4e353d14a11b346ab7865e927ce9cc9d39f5
ssalteko <ssalteko@gmail.com>
2020-04-17 18:01:32 -0700

    added more problems tolist

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ s = perf_counter_ns()
 # from Problem001 import *
 # from Problem002 import *
 # from Problem003 import *
+# from Problem004 import *
+# from Problem005 import *
+# from Problem006 import *
+# from Problem007 import *
+# from Problem008 import *
+# from Problem009 import *
+# from Problem010 import *
+# from Problem011 import *
+# from Problem012 import *
 
 t = perf_counter_ns()
 
